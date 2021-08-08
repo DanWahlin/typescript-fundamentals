1b8fc2154f6dd60a3bf041fa2e052798394c73f5
Dan Wahlin <dwahlin@xmlforasp.net>
2021-08-07 19:49:35 -0700

    Change JS logo to TS and color. Add homepage link on logo.

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,11 @@ const path = require('path');
 const fs = require('fs');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
 
-const htmlPlugins = generateHtmlPlugins('./src');
-const entryPoints = generateEntryPoints('./src', 'index.ts');
+const excludeDirs = ['css', 'images', 'lib'];
+const htmlPlugins = generateHtmlPlugins('./src', excludeDirs);
+const entryPoints = generateEntryPoints('./src', 'index.ts', excludeDirs);
 
 module.exports = {
   entry: entryPoints,
@@ -31,6 +33,11 @@ module.exports = {
   },
   plugins: [
     new MiniCssExtractPlugin({ filename: '[name]/style.css' }),
+    new CopyPlugin({
+      patterns: [
+          { from: './src/images', to: 'images' }
+      ]
+    }),
     new HtmlWebpackPlugin({
       template: './index.html',
       inject: false,
@@ -45,13 +52,13 @@ module.exports = {
 
 // Create dist/templateDir/*.html file for each templateDir found in root
 // Only include bundle/chunk associated with HTML file
-function generateHtmlPlugins(root) {
+function generateHtmlPlugins(root, excludeDirs) {
   let plugins = [];
   const rootDir = fs.readdirSync(path.resolve(__dirname, root));
   // Find directories in root folder
   rootDir.forEach((templateDir) => {
     const stats = fs.lstatSync(path.resolve(__dirname, root, templateDir));
-    if (stats.isDirectory() && templateDir !== 'css') {
+    if (stats.isDirectory() && !excludeDirs.includes(templateDir)) {
       // Read files in template directory
       const dirName = templateDir;
       const templateFiles = fs.readdirSync(path.resolve(__dirname, root, templateDir));
@@ -82,12 +89,12 @@ function generateHtmlPlugins(root) {
 // Create an entry point for each directory found in 'root'.
 // This will also create a bundle/chunk for each directory
 // and place it in the dist/[templateDir] directory.
-function generateEntryPoints(root, entryScript) {
+function generateEntryPoints(root, entryScript, excludeDirs) {
   const rootDir = fs.readdirSync(path.resolve(__dirname, root));
   let entryPoints = { css: './src/css/style.scss' };
   rootDir.forEach((templateDir) => {
     const stats = fs.lstatSync(path.resolve(__dirname, root, templateDir));
-    if (stats.isDirectory() && templateDir !== 'css') {
+    if (stats.isDirectory() && !excludeDirs.includes(templateDir)) {
       entryPoints[templateDir] = `${root}/${templateDir}/${entryScript}`;
     }
   });
