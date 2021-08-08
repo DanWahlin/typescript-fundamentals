a6ed8c751b66ce76c53d6f85e0e91ac3e6b5db86
John Papa <john@johnpapa.net>
2021-08-08 19:42:16 -0400

    adding types to the code

--- src/lib/products.ts
+++ src/lib/products.ts
@@ -1,7 +1,8 @@
+import { productsURL } from './config';
 import { Product } from './interfaces';
 
 export async function getProducts(): Promise<Product[]> {
-  const response = await fetch('../api/products.json');
-  const products: [] = await response.json();
+  const response: Response = await fetch(productsURL);
+  const products: Product[] = await response.json();
   return products;
 }
