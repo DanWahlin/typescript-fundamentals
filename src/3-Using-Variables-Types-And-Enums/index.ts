b03eabc0f2c8f9cfa0dc4b69da5127cc53ef1d0b
Dan Wahlin <dwahlin@xmlforasp.net>
2021-08-09 00:00:12 -0700

    Add module 3 code

--- src/3-Using-Variables-Types-And-Enums/index.ts
+++ src/3-Using-Variables-Types-And-Enums/index.ts
@@ -1,3 +1,33 @@
-import updateOutput from './output';
+// string, number, boolean, array, undefined, null, any
 
-updateOutput('output');
+let firstName: string | null;
+firstName = 'Dan';
+
+let age: number;
+age = 45;
+
+let hasPurchased = true;
+
+let productNames: string[] = [];
+productNames.push('Basketball');
+
+let petCount: number[] = [];
+petCount.push(5);
+ 
+console.log(firstName, age, hasPurchased, productNames, petCount);
+
+let productType = 'sports'; // homeGoods, grocery ("magic string")
+if (productType === 'sports') {
+    console.log('Found sports product type.');
+}
+
+// Using Enums 
+enum ProductType {
+    Sports,
+    HomeGoods,
+    Groceries
+}
+let pt = ProductType.Sports;
+if (pt === ProductType.Sports) {
+    console.log('Found sports product type.');
+}
--- src/3-Using-Variables-Types-And-Enums/output.ts
+++
@@ -1,6 +0,0 @@
-export default function updateOutput(id: string) {
-  const output = document.querySelector(`#${id}`);
-  if (output) {
-    output.innerHTML = 'hello';
-  }
-}
