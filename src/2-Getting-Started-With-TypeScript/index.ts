a33482463d2842ed31ed4d68a79c33c48d13a492
John Papa <johnpapa@users.noreply.github.com>
2021-08-15 23:01:21 -0400

    mod2 (#17)

--- src/2-Getting-Started-With-TypeScript/index.ts
+++ src/2-Getting-Started-With-TypeScript/index.ts
@@ -1,3 +1,11 @@
-import updateOutput from './output';
+let x = 7;
+let y = 11;
+const sum = x + y;
+const result = `The sum of ${x} and ${y} is ${sum}`;
+console.log(result);
 
-updateOutput('output');
+const output = document.querySelector('#output');
+
+if (output) {
+  output.innerHTML = result;
+}
