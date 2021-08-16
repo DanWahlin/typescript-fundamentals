e99ecff46bed2137c9852d43efcf94d1b0b9f438
John Papa <john@johnpapa.net>
2021-08-15 21:01:55 -0400

    moved functions code to functions module folder so there are no collissions

--- src/4-Creating-And-Using-Functions/output.ts
+++ src/4-Creating-And-Using-Functions/output.ts
@@ -1,4 +1,4 @@
-import { getProducts } from '../lib';
+import { productsURL } from '../lib';
 
 export default async function updateOutput(id: string) {
   const products = await getProducts();
@@ -35,4 +35,13 @@ function layoutProducts(
   });
   let productsHtml = `<ul>${items.join('')}</ul>`;
   return productsHtml;
+}
+
+async function getProducts(): Promise<
+  { id: number; name: string; icon: string }[]
+> {
+  const response: Response = await fetch(productsURL);
+  const products: { id: number; name: string; icon: string }[] =
+    await response.json();
+  return products;
 }
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,2 +1,3 @@
+export * from './config';
 export * from './interfaces';
 export * from './products';
