847e2fe850fcf7d62777ce5a52e7c4843d9551ed
John Papa <john@johnpapa.net>
2021-08-15 22:54:22 -0400

    simplify mod 2

--- src/2-Getting-Started-With-TypeScript/add.ts
+++
@@ -1,6 +0,0 @@
-export function add(x: number, y: number): number {
-  return x + y;
-}
-
-// let result = add(5, 7);
-// console.log(result);
--- src/2-Getting-Started-With-TypeScript/index.ts
+++ src/2-Getting-Started-With-TypeScript/index.ts
@@ -1,3 +1,11 @@
-import updateOutput from './output';
+let x = 10;
+let y = 15;
+const sum = x + y;
+const result = `The sum of ${x} and ${y} is ${sum}`;
+console.log(result);
 
-updateOutput('output');
+const output = document.querySelector('#output');
+
+if (output) {
+  output.innerHTML = result;
+}
