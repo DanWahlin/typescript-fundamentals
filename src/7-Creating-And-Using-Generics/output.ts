069c2a081383294d469d4abb3614874ebbac321d
John Papa <john@johnpapa.net>
2021-09-06 16:03:26 -0400

    more generic stuff

--- src/7-Creating-And-Using-Generics/output.ts
+++ src/7-Creating-And-Using-Generics/output.ts
@@ -1,9 +1,10 @@
-import { productsURL, FoodProduct } from '../lib';
+import { productsURL, FoodProduct, customersURL } from '../lib';
 
 const prefix = '🐉 ';
 
 export default async function updateOutput(id: string = 'output') {
-  const products = await getProducts();
+  // const products = await getProducts();
+  const products = await getList<FoodProduct>(productsURL);
   const output = document.querySelector(`#${id}`);
   const html = layoutProducts(products);
 
@@ -12,7 +13,7 @@ export default async function updateOutput(id: string = 'output') {
   }
 }
 
-function layoutProducts(products: FoodProduct[]) {
+function layoutProducts(products: FoodProduct[]): string {
   const items = products.map(({ id, name, icon }) => {
     const productHtml = `
     <span class="card-id">#${id}</span>
@@ -42,10 +43,137 @@ async function getProducts(): Promise<FoodProduct[]> {
   return products;
 }
 
+async function getList<T>(url: string): Promise<T[]> {
+  const response: Response = await fetch(url);
+  const items: Array<T> = await response.json();
+  console.log(`Array has ${items.length} elements`);
+  return items;
+}
+
 /************************************************
  * Learning sample code.
  ***********************************************/
 
 runTheLearningSamples();
 
-function runTheLearningSamples() {}
+async function runTheLearningSamples() {
+  // any vs generics
+  //  create a component that can work over a variety of types rather than a
+  // single one. This allows users to consume these components and use their
+  // own types.
+
+  // generics overview
+  function whatIsIt_number(arg: number): number {
+    return arg;
+  }
+  console.log(`${prefix} Generics Overview`);
+  console.log(whatIsIt_number(11));
+
+  function whatIsIt_any(arg: any): any {
+    return arg;
+  }
+  console.log(whatIsIt_any(11));
+
+  function whatIsIt_typed<Type>(arg: Type): Type {
+    return arg;
+  }
+  let n = whatIsIt_typed<number>(11);
+  console.log(n);
+  let s = whatIsIt_typed<string>('john');
+  console.log(s);
+  let b = whatIsIt_typed<boolean>(false);
+  console.log(b);
+
+  // generics on functions
+  interface Customer {
+    id: number;
+    name: string;
+  }
+
+  // ~ examine getProducts() and how it returns a Promise<FoodProduct[]>
+  // ~ examine getList() and how it returns a Promise<T[]>
+
+  async function getData() {
+    console.log(`${prefix} Generic Functions`);
+
+    const products = await getList<FoodProduct>(productsURL);
+    console.table(products);
+
+    const customers = await getList<Customer>(customersURL);
+    console.table(customers);
+  }
+  await getData();
+
+  // ~ updateOutput() for the generic getList<T>
+
+  // generic interface
+
+  interface Model<T> {
+    items: T[] | undefined;
+    getItems: () => Promise<Array<T>>;
+    getItemById: (id: number) => T | undefined;
+  }
+
+  class FoodModel implements Model<FoodProduct> {
+    public items: FoodProduct[] | undefined;
+
+    async getItems() {
+      return await getList<FoodProduct>(productsURL);
+    }
+
+    getItemById(id: number): FoodProduct | undefined {
+      if (this.items) {
+        return this.items.find((p) => (id = p.id));
+      }
+      return undefined;
+    }
+  }
+
+  const foodModel: FoodModel = new FoodModel();
+  await foodModel.getItems();
+  console.log(`${prefix} Generic Interface`);
+  console.table(foodModel.items);
+
+  // generic classes
+
+  interface HasId {
+    id: number;
+  }
+
+  // class GenericModel<T> {
+  class GenericModel<T extends HasId> {
+    public items: T[] | undefined;
+    constructor(public url: string) {}
+
+    async getItems() {
+      return await getList<T>(this.url);
+    }
+
+    getItemById(id: number): T | undefined {
+      if (this.items) {
+        return this.items.find((i) => (id = i.id));
+      }
+      return undefined;
+    }
+  }
+
+  const genericFoodModel = new GenericModel<FoodProduct>(productsURL);
+  await genericFoodModel.getItems();
+  console.log(`${prefix} Generic Class`);
+  console.table(genericFoodModel.items);
+
+  // generic constraints
+
+  // ~ GenericModel to use HasId
+
+  // uncomment the interface
+  // extend the T ==> class GenericModel<T extends HasId> {}
+
+  // ReadOnly<T>
+
+  let model: FoodModel = new FoodModel();
+  await model.getItems();
+  let roFoodItem: Readonly<FoodProduct | undefined> = model.getItemById(10);
+  // roFoodItem.name = 'some name';
+  // roFoodItem.icon = 'some icon';
+}
